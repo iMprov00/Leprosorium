15f32da22cc64f0d7b74174aa939eb4f18a94fd2
iMprov <improv00@mail.ru>
2025-04-28 14:47:12 +0700

    Добавлено подключение БД

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true
+end
+
+before do 
+	init_db
+end
+
+configure do 
+	init_db
+	@db.execute 'CREATE TABLE IF NOT EXISTS Posts 
+		(id integer PRIMARY KEY AUTOINCREMENT,
+		"ctreated_date" date,
+		content text)'
+
+end
 
 get '/' do 
 
@@ -17,7 +36,7 @@ end
 get '/new' do 
 
 
-	erb :new_post
+	erb :new
 
 end
 
